293a9ecfaae3add32e4981191b31d75fc59fee96
odouimi <o.douimi@attijariwafa.com>
2025-05-22 15:28:41 +0100

    test

--- app/scrapers/santander.py
+++ app/scrapers/santander.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 import time
 import logging
 from typing import Dict, List
